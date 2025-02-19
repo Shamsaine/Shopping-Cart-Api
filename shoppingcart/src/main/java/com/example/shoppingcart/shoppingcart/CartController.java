222828e4b92326ad6a29ea8d55f274faeebc824e
Shamsudeen Ibrahim <shamsain3@gmail.com>
2025-02-19 22:57:12 +0100

    updated the file to adjust and use constructor injection

--- shoppingcart/src/main/java/com/example/shoppingcart/shoppingcart/CartController.java
+++ shoppingcart/src/main/java/com/example/shoppingcart/shoppingcart/CartController.java
@@ -8,25 +8,34 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.bean.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Autowired;
 import java.util.List;
 
 @Controller
 public class CartController {
 
+    //what it was before the dependency injection
+    //@Autowired
+    //public CartService cartservice;
+
+    private final CartService cartService;
+
+    // Using constructor injection to replace the initial design
     @Autowired
-    public CartService cartservice;
+    public CartController(CartService cartService) {
+        this.cartService = cartService;
+    }
 
     @GetMapping("/cart")
     @ResponseBody
-    public List<CartModel> getAll(){
-        return cartservice.getCartList();
+    public List<CartModel> getAll() {
+        return cartService.getCartList();
     }
     
     @PostMapping("/cart")
     @ResponseBody
-    public String addCartItem(@ResponseBody CartModel cart) {
-        cartservice.addCartItem(cart);
+    public String addCartItem(@RequestBody CartModel cart) {
+        cartService.addCartItem(cart);
         return "item added successfully";
     }
 
@@ -34,14 +43,14 @@ public class CartController {
     @ResponseBody
     public String updateCartItem(@PathVariable int id, @RequestBody CartModel cart) {
         cart.setId(id);
-        cartservice.updateCartItem(cart);
+        cartService.updateCartItem(cart);
         return "item updated successfully";
     }
 
     @DeleteMapping("/cart/{id}")
     @ResponseBody
     public String deleteCartItem(@PathVariable int id) {
-        cartservice.deleteCartItem(id);
+        cartService.deleteCartItem(id);
         return "item deleted successfully";
     }
 }
